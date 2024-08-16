8c9fc64a830817aa6d39c302e6bed67709c73a97
EvanLiSH <156528490+EvanLiSH@users.noreply.github.com>
2024-08-16 19:22:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"os/signal"
 	"syscall"
 
-	C "github.com/Dreamacro/clash/constant"
-	"github.com/Dreamacro/clash/hub"
-	"github.com/Dreamacro/clash/proxy/http"
-	"github.com/Dreamacro/clash/proxy/socks"
-	"github.com/Dreamacro/clash/tunnel"
+	C "github.com/EvanLiSH/clash/constant"
+	"github.com/EvanLiSH/clash/hub"
+	"github.com/EvanLiSH/clash/proxy/http"
+	"github.com/EvanLiSH/clash/proxy/socks"
+	"github.com/EvanLiSH/clash/tunnel"
 
 	log "github.com/sirupsen/logrus"
 )
